73f6afd2a1aa38ebfbdae091b1011ab2444b58bb
rambabu.d <rambabu.d@outlook.com>
2014-10-16 23:22:22 +0530

    Added registered template handler for haml

--- lib/haml_ui.rb
+++ lib/haml_ui.rb
@@ -6,23 +6,15 @@ module HamlUi
   	def self.call(template)
   		binding.pry
 
-  	  options = Haml::Template.options.dup
-      # if (ActionPack::VERSION::MAJOR >= 4) && template.respond_to?(:type)
-      #   options[:mime_type] = template.type
-      # elsif template.respond_to? :mime_type
-      #   options[:mime_type] = template.mime_type
-      # end
-      # options[:filename] = template.identifier
-
-
-  		@ram = "rambabu"
-  		@tem = template
-  		@king = @source.to_s
   		#template.source.inspect.html_safe
-  		#@ce = "%h1 Rambabu King"
-  		@hui_source = template.source
-  		#ActionView::Template.registered_template_handler(:haml).call(template)
-  		Haml::Engine.new(@hui_source, options).compiler.precompiled_with_ambles([])
+      # if (ActionPack::VERSION::MAJOR >= 4) && template.respond_to?(:type)
+        @haml_ = ActionView::Template.registered_template_handler(:haml)
+        @haml_compiled = @haml_.call(template)    
+        @gen = <<-SOURCE
+  		begin;#{@haml_compiled};end
+  		SOURCE
+      #s.split /(?=[A-Z])/
+  		@gen.gsub(/largeButton/, "btn btn-lg")
     end
 
   end
